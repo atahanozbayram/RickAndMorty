50a96c24796e89fde82cb107446d8729f01a4951
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-07-16 18:35:55 +0300

    Moved App.js to components folder, added sketchy design in the App component for the whole app

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App(props) {
+	return (
+		<div>
+			{' '}
+			{/* Outer div of all */}
+			<div>
+				{' '}
+				{/* Top bar and hero area. */}
+				<div> {/* top bar */}</div>
+				<img /> {/* Image */}
+			</div>
+			<div>
+				{' '}
+				{/* section for listing cards and filtering */}
+				<div>{/* section for filtering elements */}</div>
+				<div>{/* section for cards */}</div>
+			</div>
+		</div>
+	);
+}
+
+export { App as default };
