1f499e1d4765961b221b4c8b0c4db291648357db
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-08-06 21:55:23 +0300

    Changed BrowserRouter to HashRouter for compatibility of netlify

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 // Dependencies:
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // Custom components:
 import Filter from '../components/Filter';
